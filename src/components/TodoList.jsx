13e993e2541f6fd6cc915d8b3c5bfbcb08c5c1f9
KelvinTang <tangke3@oocl.com>
2025-09-12 15:18:09 +0800

    feat: add done status change

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,18 @@
 import {TodoContext} from "../contexts/TodoContext";
 import {useContext} from "react";
-import "../TodoList.css";
-
+import "./TodoList.css";
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext)
+
+    function toggleDone(id) {
+        dispatch({type: "DONE", id: id})
+    }
+
     return (<div className={"todo-group"}>
             <div>This is the TodoList Component.</div>
             {
-                state.map(todo => {
-                    return  <div className={"todo-item"}>{todo.text}</div>
+                state.map(({id, text, done}) => {
+                    return  <div className={"todo-item"} onClick={() => toggleDone(id)}>{text} {done+''}</div>
                 })
             }
         </div>

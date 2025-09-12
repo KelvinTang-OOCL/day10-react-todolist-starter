d956636151ff0eeb180956e113a5b934ff862fff
KelvinTang <tangke3@oocl.com>
2025-09-12 15:04:06 +0800

    debug: fix className of div

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,14 @@
 import {TodoContext} from "../contexts/TodoContext";
 import {useContext} from "react";
+import "../TodoList.css";
 
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext)
-    return (<div>
+    return (<div className={"todo-group"}>
             <div>This is the TodoList Component.</div>
             {
                 state.map(todo => {
-                    return  <div>{todo.text}</div>
+                    return  <div className={"todo-item"}>{todo.text}</div>
                 })
             }
         </div>
